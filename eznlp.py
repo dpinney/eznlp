011d059455899fbe756c77eb6552cfb84418743e
David Pinney <david@pinney.org>
2021-04-30 17:50:44 -0400

    test update remove star import

--- eznlp.py
+++ eznlp.py
@@ -145,7 +145,7 @@ def _google_answer():
 	# Google custom search api approach
 	#!pip install google-api-python-client
 	from googleapiclient.discovery import build
-	from keys import *
+	from keys import my_api_key,my_cse_id
 	def google_search(search_term, api_key, cse_id, **kwargs):
 		service = build("customsearch", "v1", developerKey=api_key)
 		res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
