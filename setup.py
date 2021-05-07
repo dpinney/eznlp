bfda730a315d383de76d1cda116a447b4da283f7
David Pinney <david@pinney.org>
2021-05-07 10:48:14 -0400

    Long desc in setup

--- setup.py
+++ setup.py
@@ -4,9 +4,18 @@ Install eznlp.
 
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
 	name='eznlp',
-	version='1.0.0',
+	version='1.0.1',
+	description='Easy Natural Languge Processing Library',
+	long_description_content_type='text/markdown',
+	long_description=long_description,
+	author='David Pinney',
+	author_email='david@pinney.org',
+	url='https://github.com/dpinney/eznlp/',
 	py_modules=['eznlp'],
 	install_requires = open("requirements.txt").readlines(),
 )
